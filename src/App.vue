19cd580e4c7daf4634684e8417572355f2b2d7f4
Adarsh Srivastava <adarsh.srivastava@deskera.org>
2020-02-18 17:43:08 +0530

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,148 @@
+<template>
+<div>
+  <nav class="navbar is-fixed-top">
+  </nav>
+  <p>Hello World</p>
+<section class="hero is-link is-fullheight-with-navbar">
+  <div class="hero-body">
+    <div class="container">
+<div class="box">
+   <article class="media">
+    <div class="media-content">
+      <div class="content">
+         <img src="https://via.placeholder.com/480" alt="Image">
+        <p>
+          <strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small>
+          <br>
+          Nullam condimentum luctus turpis.
+        </p>
+      </div>
+      <nav class="level is-mobile">
+        <div class="level-left">
+          <a class="level-item" aria-label="reply">
+
+            <i class="fas fa-reply" aria-hidden="true"></i>
+
+          </a>
+          <a class="level-item" aria-label="retweet">
+
+            <i class="fas fa-retweet" aria-hidden="true"></i>
+
+          </a>
+          <a class="level-item" aria-label="like">
+
+            <i class="fas fa-heart" aria-hidden="true"></i>
+
+          </a>
+        </div>
+        <div class="level-right">
+          <a class="level-item" aria-label="like">
+            <i class="fas fa-heart" aria-hidden="true"></i>
+          </a>
+        </div>
+      </nav>
+    </div>
+  </article>
+</div>
+<div class="box"></div>
+<div class="box"></div>
+<div class="box"></div>
+<div class="box"></div>
+<div class="box"></div>
+<div class="box"></div>
+<div class="box"></div>
+    </div>
+  </div>
+</section>
+<nav class="bd-navbar navbar is-fixed-bottom" role="navigation" aria-label="main navigation">
+<div class="navbar-brand level">
+  <a role="button" class="navbar-item has-text-centered">
+    <div>
+    <p class="heading">Tweets</p>
+    <p class="heading">3,456</p>
+  </div>
+  </a>
+
+  <a role="button" class="navbar-item has-text-centered">
+    <div>
+    <p class="heading">Tweets</p>
+    <p class="heading">3,456</p>
+  </div>
+  </a>
+
+  <a role="button" class="navbar-item has-text-centered">
+    <div>
+    <p class="heading">Tweets</p>
+    <p class="heading">3,456</p>
+  </div>
+  </a>
+
+  <a role="button" class="navbar-item has-text-centered">
+    <div>
+    <p class="heading">Tweets</p>
+    <p class="heading">3,456</p>
+  </div>
+  </a>
+</div>
+
+</nav>
+
+</div>
+<!-- <nav class=" navbar level is-mobile is-fixed-bottom">
+  <a class="level-item has-text-centered">
+    <div>
+      <p class="heading">Tweets</p>
+      <p class="title">3,456</p>
+    </div>
+  </a>
+  <a class="level-item has-text-centered">
+    <div>
+      <p class="heading">Following</p>
+      <p class="title">123</p>
+    </div>
+  </a>
+  <a class="level-item has-text-centered">
+    <div>
+      <p class="heading">Followers</p>
+      <p class="title">456K</p>
+    </div>
+  </a>
+  <a class="level-item has-text-centered">
+    <div>
+      <p class="heading">Likes</p>
+      <p class="title">789</p>
+    </div>
+  </a>
+</nav> -->
+
+  <!-- <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div> -->
+</template>
+
+<style>
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+} */
+</style>
